7593c16a58487ce033a2205e91eae82cd5a2fe59
Lakshmi M <148333359+smaRttech15@users.noreply.github.com>
2024-11-18 09:29:39 +0530

    2 week assignments and programs uploaded

---
+++ SECOND WEEK/numpy3.py
@@ -0,0 +1,8 @@
+import numpy as np
+# nan is not a number
+print(0 * np.nan)
+print(np.nan == np.nan)
+print(np.inf > np.nan)
+print(np.nan - np.nan)
+print(np.nan in set([np.nan]))
+print(0.3 == 3 * 0.1)
